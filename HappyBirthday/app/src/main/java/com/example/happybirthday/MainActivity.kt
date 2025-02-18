ae1eb14c4b61dc7a2e09d932aa29d7812955b21e
Mach-web <mkanderson771@gmail.com>
2025-02-18 11:46:20 +0300

    Padding and alignment

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,9 +16,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.movableContentOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -47,18 +51,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText( modifier: Modifier = Modifier, text: String = "Happy Birthday MachTechie!!!", from: String) {
-    Column {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
         Text(
             text = text,
-            modifier = modifier,
+            modifier = Modifier.padding(8.dp),
             fontSize = 100.sp,
-            lineHeight = 100.sp
+            lineHeight = 100.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
-            fontSize = 100.sp,
-            modifier = modifier,
-            lineHeight = 100.sp
+            fontSize = 36.sp,
+            modifier = Modifier.padding(16.dp).align(alignment = Alignment.End),
+            lineHeight = 100.sp,
+            textAlign = TextAlign.Center,
+
         )
     }
 }
