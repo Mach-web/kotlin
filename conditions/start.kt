e491e30ddf8c1c4c850a6425eabbad10546cc231
Mach-web <mkanderson771@gmail.com>
2025-02-28 23:11:34 +0300

    Using when

--- conditions/start.kt
+++ conditions/start.kt
@@ -1,11 +1,21 @@
+// kotlinc start.kt -include-runtime -d start.jar
+// java -jar start.jar
 fun main() {
-    val trafficLightColor = "Yellow"
+    val trafficLightColor = "MAroon"
 
-    if (trafficLightColor == "Red") {
-        println("Stop")
-    } else if (trafficLightColor == "Yellow"){
-        println("Slow")
-    } else {
-        println("Go")
+    // if (trafficLightColor == "Red") {
+    //     println("Stop")
+    // } else if (trafficLightColor == "Yellow"){
+    //     println("Slow")
+    // } else if (trafficLightColor == "Green"){
+    //     println("Go")
+    // } else {
+    //     println("Invalid Color")
+    // }
+    when(trafficLightColor){
+        "Red" -> println("Stop")
+        "Yellow" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid Color")
     }
 }
